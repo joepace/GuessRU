43abe59cd079f88b346b0f49a4e3f59b72c3488a
Andrew Hernandez <andrewh717@gmail.com>
2018-10-07 03:58:43 -0400

    Combining everything into the StreetView component
    Instead of using a separate component for the Guess aspect, we are just going to have a form in the StreetView component so that data is all together

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import './style.css';
 import StreetView from './components/StreetView';
-import Guess from './components/Guess';
 
 class App extends Component {
   render() {
@@ -10,10 +9,8 @@ class App extends Component {
       <div className="App">   
         <div className="container">
           <h1>Hello, HackRU!</h1>
-          <div className="row">
+          
             <StreetView/>
-            <Guess/>
-          </div>
         </div>
       </div>
     );
--- src/components/StreetView.js
+++ src/components/StreetView.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Map from './Map'
 import Panorama from './Panorama'
 
 class StreetView extends Component {
@@ -7,47 +6,77 @@ class StreetView extends Component {
         const coords = generateLocation();
         const location = {lat: coords[0], lng: coords[1]};
         return (
-            <div className="streetView col-6">
-                {/* StreetView section goes here */}
-                <Panorama 
-                    id="myPanorama"
-                    options={{
-                        position: location,
-                        pov: {
-                            heading: 34,
-                            pitch: 10
-                        },
-                        addressControl: false
-                    }}
-                />
-            </div>        
+            <div className="row">
+                <div className="streetView col-6">
+                    {/* StreetView section goes here */}
+                    <Panorama 
+                        id="myPanorama"
+                        options={{
+                            position: location,
+                            pov: {
+                                heading: 34,
+                                pitch: 10
+                            },
+                            addressControl: false
+                        }}
+                    />
+                </div>
+                <div className="guess col-6">
+                    <h2>Which campus is this?</h2>
+                    <form action="">
+                        <input type="radio" name="campus" value="1"/> College Ave <br/>
+                        <input type="radio" name="campus" value="2"/> Livingston <br/>
+                        <input type="radio" name="campus" value="3"/> Busch <br/>
+                        <input type="radio" name="campus" value="4"/> Cook/Douglass
+                    </form>
+                    
+                </div>       
+            </div>
         );
     }
+}
+
+function checkAnswer(guess) {
+    if (campus === guess) {
+        // Call function to display "Correct!"
+    } else {
+        // Display correct answer
+    }
+    // Call playAgain function
+}
+
+function correct() {
+    // Display a "Correct!" message
+}
+
+function playAgain() {
+    // Refreshes the page? idk
 }
 
 function generateCampus() {
     return Math.floor(Math.random() * 3 + 1);
 }
 
+// Global campus ID
+const campus = generateCampus();
 function generateLocation() {
-    let campus = generateCampus();
     console.log(campus);
     let lat;
     let long;
     switch (campus) {
-        case 1:
+        case 1: // College Ave
             lat =  Math.random()*(40.505717-40.498368) + 40.498368;
             long = Math.random()*(-74.445459+74.447196) -74.447196;
             break;
-        case 2:
+        case 2: // Livingston
             lat = Math.random()*(40.525217-40.518432) + 40.518432;
             long = Math.random()*(-74.432783+74.447196) -74.447196;
             break;
-        case 3:
+        case 3: // Busch
             lat = Math.random()*(40.528708-40.518432) + 40.518432;
             long = Math.random()*(-74.453373+74.467771) -74.467771;
             break;
-        case 4:
+        case 4: // Cook/Douglass
             lat = Math.random()*(40.485598-40.475704) + 40.475704;
             long = Math.random()*(-74.427856+74.437336) -74.437336;
             break;
