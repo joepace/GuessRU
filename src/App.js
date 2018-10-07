ec131398d37194dec652e69f7f430bbf3de93527
Andrew Hernandez <andrewh717@gmail.com>
2018-10-07 03:13:07 -0400

    Full page div ayyy

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">   
-        <h1>Hello, HackRU!</h1>
         <div className="container">
+          <h1>Hello, HackRU!</h1>
           <div className="row">
             <StreetView/>
             <Guess/>
--- src/components/Guess.js
+++ src/components/Guess.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Guess extends Component {
     render() {
         return (
-            <div className="guess col-4">
+            <div className="guess col-6">
                 {/* Guess section goes here */}
             </div>
         );
